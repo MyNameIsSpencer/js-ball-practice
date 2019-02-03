9fb597ef56207e2b50f06c4eab43e29cef04ce2a
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-02-02 21:19:37 -0500

    ball bounces off paddle and executes game over on hit floor

--- jscript1.js
+++ jscript1.js
@@ -10,9 +10,9 @@ var rightPressed = false;
 var leftPressed = false;
 
 var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 1;
-var dy = -1;
+var y = canvas.height-80;
+var dx = 3;
+var dy = -3;
 var ballRadius = 10;
 
 document.addEventListener("keydown", keyDownHandler, false);
@@ -95,10 +95,19 @@ function drawBall() {
     ctx.closePath();
 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-      dx = -dx;
+        dx = -dx;
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+
+    if(y + dy < ballRadius) {
       dy = -dy;
+    } else if(y + dy > canvas.height-50-ballRadius) {
+      if(x > paddleX && x < paddleX + paddleWidth) {
+        dy = -dy;
+      }
+      else if(y >= canvas.height-ballRadius) {
+        alert("GAME OVER");
+        document.location.reload();
+      }
     }
 }
 
@@ -114,87 +123,3 @@ function draw() {
 
 
 var interval = setInterval(draw, 10);
-
-
-
-
-
-
-
-
-
-//
-//
-// var canvas = document.getElementById("myCanvas");
-// var ctx = canvas.getContext("2d");
-// var ballRadius = 10;
-// var x = canvas.width/2;
-// var y = canvas.height-30;
-// var dx = 2;
-// var dy = -2;
-// var paddleHeight = 10;
-// var paddleWidth = 75;
-// var paddleX = (canvas.width-paddleWidth)/2;
-// var rightPressed = false;
-// var leftPressed = false;
-//
-// document.addEventListener("keydown", keyDownHandler, false);
-// document.addEventListener("keyup", keyUpHandler, false);
-//
-// function keyDownHandler(e) {
-//     if(e.key == "Right" || e.key == "ArrowRight") {
-//         rightPressed = true;
-//     }
-//     else if(e.key == "Left" || e.key == "ArrowLeft") {
-//         leftPressed = true;
-//     }
-// }
-//
-// function keyUpHandler(e) {
-//     if(e.key == "Right" || e.key == "ArrowRight") {
-//         rightPressed = false;
-//     }
-//     else if(e.key == "Left" || e.key == "ArrowLeft") {
-//         leftPressed = false;
-//     }
-// }
-//
-// function drawBall() {
-//     ctx.beginPath();
-//     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-//     ctx.fillStyle = "#0095DD";
-//     ctx.fill();
-//     ctx.closePath();
-// }
-// function drawPaddle() {
-//     ctx.beginPath();
-//     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-//     ctx.fillStyle = "#0095DD";
-//     ctx.fill();
-//     ctx.closePath();
-// }
-//
-// function draw() {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     drawBall();
-//     drawPaddle();
-//
-//     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-//         dx = -dx;
-//     }
-//     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-//         dy = -dy;
-//     }
-//
-//     if(rightPressed && paddleX < canvas.width-paddleWidth) {
-//         paddleX += 7;
-//     }
-//     else if(leftPressed && paddleX > 0) {
-//         paddleX -= 7;
-//     }
-//
-//     x += dx;
-//     y += dy;
-// }
-//
-// setInterval(draw, 10);
